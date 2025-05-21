26737818fddba932ea3dec78edda8c2f80c5f32a
Bhanuprasad17 <bhanuprasadsuram0018@gmail.com>
2025-05-21 21:58:52 +0530

    pathname and query param

--- index.js
+++ index.js
@@ -1,41 +1,27 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const url = require('url')
 
-const data = {
-    name : 'Bhanu',
-    role : 'developer',
-    language : 'javascript'
-}
+let server = http.createServer((req,res) =>{
+    // console.log(url.parse(req.url))
 
-const server = http.createServer((req,res)=>{
-    // res.writeHead(200,{'content-type' : 'text/html'})
-    if(req.url === '/'){
-    // res.end('welcom to home page')
-    // res.end(`
-    //       <html>
-    //           <head><title>Node Server</title></head>
-    //           <body><h1>Hello from node.js</h1></body>
-    //       </html>
-    //     `)
-    fs.readFile('./index.html',(err,data)=>{
-        if(err){
-             res.statusCode = 500;
-            res.end('Error loading HTML');
-        }else{
-             res.setHeader('Content-Type', 'text/html');
-             res.end(data);
-        }
-    })
-  }
-  else if(req.url === '/about'){
-    res.end('about us page')
-  }
-  else{
-    res.statusCode == 404
-    res.end('page not found')
-  }
+    const parsedUrl = url.parse(req.url, true)
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query
+    console.log(pathname)
+    console.log(query)
+    if(pathname === '/'){
+        res.end('welcome to homepage')
+    }
+    else if(pathname === '/user'){
+        let name = query.name
+        let role = query.role
+        res.end(`Hello ${name}, your role is ${role}`)
+    }else{
+        res.statusCode = 400
+        res.end('page not found')
+    }
 })
 
 server.listen(3000,()=>{
-    console.log('server is running on port 3000')
+    console.log(`server is running on port ${3000}`)
 })
